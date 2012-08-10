de2b8530a61bf043b46bcbe94b496284051df3b8
Gert Vrebos <gert.vrebos@tactics.be>
2012-08-10 21:31:41 +0200

    [FEATURE] Interface and Abstract class for ColumnTypeExtensions

---
+++ ColumnTypeExtensionInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tactics\TableBundle;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Tactics\TableBundle\ColumnInterface;
+
+/**
+ * Description of TableTypeExtensionInterface
+ *
+ * @author Gert Vrebos <gert.vrebos at tactics.be>
+ */
+
+interface ColumnTypeExtensionInterface
+{
+    public function execute(ColumnInterface $column, array &$row, array &$cell);
+
+    /**
+     * Overrides the default options from the extended type.
+     *
+     * @param OptionsResolverInterface $resolver The resolver for the options.
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver);
+
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType();
+}
+
