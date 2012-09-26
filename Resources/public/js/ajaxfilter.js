d5d7deec6496a7cd131492b5933cafaff3ba7118
Gert Vrebos <gert.vrebos@tactics.be>
2012-09-26 17:03:18 +0200

    Added support for ajax loading indication on tables

--- Resources/public/js/ajaxfilter.js
+++ Resources/public/js/ajaxfilter.js
@@ -1,20 +1,24 @@
-(function($) {
-    $(function($) {
-        $('.form-filter').live('submit', function() {
-            var form = $(this);
+jQuery(function($) {
+    $('.form-filter').live('submit', function() {
+        var form = $(this);
+        var target = '#'+form.attr('data-target');
+        
+        $(target).closest('.widget').addClass('ajax-loading');
 
-            $.ajax({
-                url:      form.attr('action'),
-                type:     "POST",
-                data:     form.serialize(), 
-                dataType: "html",
-                success:  function(html) {
-                    var target = '#'+form.attr('data-target');
-                    $(target).replaceWith($(target, $(html)));
-                }
-            });
-
-            return false;
+        $.ajax({
+            url:      form.attr('action'),
+            type:     "POST",
+            data:     form.serialize(), 
+            dataType: "html",
+            success:  function(html) {
+                $(target).replaceWith($(target, $(html)));
+            },
+            complete: function()
+            {
+                $(target).closest('.widget').removeClass('ajax-loading');
+            }
         });
+
+        return false;
     });
-})(jQuery);
+});
--- Resources/public/js/ajaxtablesort.js
+++ Resources/public/js/ajaxtablesort.js
@@ -1,19 +1,23 @@
-(function($) {
-    $(function($) {
-        $('.a-filter').live('click', function() {
-            var element = $(this);
-            var target  = $('#results');
+jQuery(function($) {
+    $('.a-filter').live('click', function() {
+        var element = $(this);
+        var target  = '#results';
+        
+        $(target).closest('.widget').addClass('ajax-loading');
 
-            $.ajax({
-                url:      element.attr('href'),
-                type:     "POST",
-                dataType: "html",
-                success:  function(html) {
-                    target.replaceWith($('#results', $(html)));
-                }
-            });
-
-            return false;
+        $.ajax({
+            url:      element.attr('href'),
+            type:     "POST",
+            dataType: "html",
+            success:  function(html) {
+                $(target).replaceWith($(target, $(html)));
+            },
+            complete: function()
+            {
+                $(target).closest('.widget').removeClass('ajax-loading');
+            }
         });
+
+        return false;
     });
-})(jQuery);
+});
