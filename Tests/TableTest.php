023975c5a796d03242e9816995621fe67f1c3d04
Aaron Muylaert <aaron@tactics.be>
2012-10-05 17:48:25 +0200

    Add some more tests.

--- Tests/TableTest.php
+++ Tests/TableTest.php
@@ -9,17 +9,99 @@ use Tactics\TableBundle\Column;
 class TableTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @covers Tactics\TableBundle\Table::add
+     * @var TableInterface A TableInterface instance.
      */
-    public function testAdd()
+    private $table;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
     {
-        $table = new Table('table'); 
-        $table->add(new Column('Foo', new ColumnHeader('Foo')));
+        $this->table = new Table('table'); 
+    }
 
-        $columns = $table->getColumns();
-        $column = end($columns);
+    /**
+     * @covers Tactics\TableBundle\Table::add
+     * @covers Tactics\TableBundle\Table::getColumns
+     */
+    public function testAddAndGetColumns()
+    {
+        $column = new Column('Foo', new ColumnHeader('Foo'));
+        $this->table->add($column);
 
-        $this->assertEquals('Foo', $column->getName());
+        $columns = $this->table->getColumns();
+
+        $this->assertEquals($column, end($columns));
+    }
+
+    /**
+     * @covers Tactics\TableBundle\Table::setRows
+     * @covers Tactics\TableBundle\Table::getRows
+     */
+    public function testSetAndGetRows()
+    {
+        $rows = array(
+            array('Foo', 'Bar', 'Baz')
+        );
+
+        $this->table->setRows($rows);
+
+        $this->assertEquals($rows, $this->table->getRows());
+    }
+
+    /**
+     * @covers Tactics\TableBundle\Table::offsetGet
+     */
+    public function testOffsetGet()
+    {
+        $column = new Column('Foo', new ColumnHeader('Foo'));
+
+        $this->table->add($column);
+
+        $this->assertEquals($column, $this->table->offsetGet('Foo'));
+        $this->assertNull($this->table->offsetGet('Bar'));
+    }
+
+    /**
+     * @covers Tactics\TableBundle\Table::offsetExists
+     */
+    public function testOffsetExists()
+    {
+        $column = new Column('Foo', new ColumnHeader('Foo'));
+
+        $this->table->add($column);
+
+        $this->assertEquals(false, $this->table->offsetExists('foo'));
+        $this->assertEquals(true, $this->table->offsetExists('Foo'));
+    }
+
+    /**
+     * @depends testOffsetGet
+     * @covers Tactics\TableBundle\Table::offsetExists
+     */
+    public function testOffsetSet()
+    {
+        $column = new Column('Foo', new ColumnHeader('Foo'));
+
+        $this->table->offsetSet($column->getName(), $column);
+
+        $this->assertEquals($column, $this->table->offsetGet($column->getName()));
+    }
+
+    /**
+     * @depends testOffsetSet
+     * @depends testOffsetExists
+     * @covers Tactics\TableBundle\Table::offsetExists
+     */
+    public function testOffsetUnset()
+    {
+        $column = new Column('Foo', new ColumnHeader('Foo'));
+
+        $this->table->offsetSet($column->getName(), $column);
+
+        $this->table->offsetUnset($column->getName());
+
+        $this->assertFalse($this->table->offsetExists('Foo'));
     }
 }
-
