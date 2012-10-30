8e33c7cdd07f8145377f82d32fc3cc0638035584
Aaron Muylaert <aaron@tactics.be>
2012-10-30 11:16:36 +0100

    Add required query_builder option and set the QueryBuilder instance.

--- Extension/Builder/DoctrineTableBuilder.php
+++ Extension/Builder/DoctrineTableBuilder.php
@@ -2,6 +2,46 @@
 
 namespace Tactics\TableBundle\Extension\Builder;
 
+use Tactics\TableBundle\TableBuilder;
+use Tactics\TableBundle\TableFactoryInterface;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
 class DoctrineTableBuilder extends TableBuilder
 {
+    protected $columns = array();
+
+    /**
+     * @var $queryBuilder Doctrine\ORM\QueryBuilder A QueryBuilder instance.
+     */
+    protected $queryBuilder = null;
+
+    /**
+     * Namespace used by \Tactics\Bundle\TableBundle\ModelCriteriaFilter\ModelCriteriaSorter
+     *
+     * @var $sorterNamespace string The sorter namespace.
+     */
+    protected $sorterNamespace = null;
+
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct($name, $type = '', TableFactoryInterface $factory, array $options = array())
+    {
+        parent::__construct($name, $type, $factory, $options);
+
+        $this->queryBuilder = $this->options['query_builder'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setRequired(array('query_builder'));
+    }
 }
