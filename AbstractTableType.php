7e05c44d2c29a4f52c9f445544f3c20432b4f3a0
Aaron Muylaert <aaron@tactics.be>
2012-11-16 16:13:01 +0100

    Fix setDefaultOptions in TableTypeInterface and AbstracTableType.

--- AbstractTableType.php
+++ AbstractTableType.php
@@ -1,11 +1,8 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Tactics\TableBundle;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * Description of AbstractTableType
