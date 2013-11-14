834fe8a4fdbde0b8e56213fb667ab5fa30d6fc81
Bart Ruysseveldt <bart@tactics.be>
2013-11-14 11:35:59 +0100

    fix icons

--- Extension/Type/ActionsColumn.php
+++ Extension/Type/ActionsColumn.php
@@ -29,20 +29,20 @@ class ActionsColumn extends Column
         foreach ($this->options['actions'] as $action => &$options) {
             $options = $actionsResolver->resolve($options);
         }
-        
+
         // todo with css parser
         // $css = new HTML_CSS();
         // $css->parseString($this->getHeader()->getOption('attributes'));
         // $css->setStyle('width', (count($this->actions) * 25) . 'px');
         // $this->getHeader()->setOption('attributes', $css->toInline());
-        
-        
+
+
         $headerAttributes = $this->getHeader()->getOption('attributes');
         $headerAttributes = $headerAttributes ? $headerAttributes : array();
         $headerAttributes['style'] = (isset($headerAttributes['style']) ? $headerAttributes['style'] : '') . sprintf('; width: %upx;', count($this->options['actions']) * 25);
         $headerAttributes['style'] .= 'text-align: center;';
         $this->getHeader()->setOption('attributes', $headerAttributes);
-        
+
         $attributes = $this->getOption('attributes');
         $attributes = $attributes ? $attributes : array();
         $attributes['style'] = (isset($attributes['style']) ? $attributes['style'] : '') . '; text-align: center;';
@@ -55,11 +55,11 @@ class ActionsColumn extends Column
     public function getCell($row)
     {
         $cell = parent::getCell($row);
-        
+
         // todo nested resolvers?
         foreach ($this->options['actions'] as $action => $options) {
             $cell['actions'][$action] = $options;
-            
+
             switch($action)
             {
                 case 'delete':
@@ -67,45 +67,53 @@ class ActionsColumn extends Column
                     if (
                         (! isset($cell['actions'][$action]['disabled']) || ! $cell['actions'][$action]['disabled'])
                         && isset($row['_object']) && method_exists($row['_object'], 'isDeletable')) {
-                        
+
                         if (! $row['_object']->isDeletable()) {
                             $cell['actions'][$action]['disabled'] = true;
                             $className = strtolower(join('', array_slice(explode('\\', get_class($row['_object'])), -1)));
                             $cell['actions'][$action]['title'] = 'This ' . $className . ' cannot be deleted.';
                         }
                     }
-                    
+
                     // default icon
                     if (! isset($cell['actions'][$action]['icon']))
                     {
-                        $cell['actions'][$action]['icon'] = 'trash';
-                    }                        
-                    
+                        $cell['actions'][$action]['icon'] = 'fa fa-trash-o';
+                    }
+
                     break;
-                    
+
                 case 'show':
                     // default icon
                     if (! isset($cell['actions'][$action]['icon']))
                     {
-                        $cell['actions'][$action]['icon'] = 'search';
+                        $cell['actions'][$action]['icon'] = 'fa fa-search';
                     }
-                    
+
                     break;
-                    
+
+                case 'edit':
+                    // default icon
+                    if (! isset($cell['actions'][$action]['icon'])) {
+                        $cell['actions'][$action]['icon'] = 'fa fa-edit';
+                    }
+
+                    break;
+
             }
-            
+
             // Default action title
             if (! isset($cell['actions'][$action]['title']))
             {
                 $cell['actions'][$action]['title'] = ucfirst($action);
             }
-            
+
             // Default action icon
             if (! isset($cell['actions'][$action]['icon']))
             {
                 $cell['actions'][$action]['icon'] = $action;
             }
-            
+
             if (isset($cell['actions'][$action]['disabled']))
             {
                 $disabled = $cell['actions'][$action]['disabled'];
@@ -113,15 +121,15 @@ class ActionsColumn extends Column
                 if (is_callable($disabled)) {
                     if ($disabled($row)) {
                         $cell['actions'][$action]['attributes']['class'] = trim(
-                            $cell['actions'][$action]['attributes']['class'] . 
-                            ' ' . 
+                            $cell['actions'][$action]['attributes']['class'] .
+                            ' ' .
                             'disabled'
                         );
                     }
                 } elseif ($disabled) { // BC, deprecated.
                     $cell['actions'][$action]['attributes']['class'] = trim(
-                        $cell['actions'][$action]['attributes']['class'] . 
-                        ' ' . 
+                        $cell['actions'][$action]['attributes']['class'] .
+                        ' ' .
                         'disabled'
                     );
                 }
@@ -135,7 +143,7 @@ class ActionsColumn extends Column
                     $cell['actions'][$action]['disabled'] = true;
                 }
             }
-            
+
             // BC
             if (isset($options['route_param'])) {
                 $cell['actions'][$action]['route_name'] = $options['route'];
@@ -154,14 +162,14 @@ class ActionsColumn extends Column
                 throw new TableException('Invalid route configuration for action in actionsColumn.');
             }
         }
-        
+
         return $cell;
-    }    
-    
+    }
+
     /**
      * @return array The actions.
      */
-    public function getActions() 
+    public function getActions()
     {
         return $this->options['actions'];
     }
@@ -187,7 +195,7 @@ class ActionsColumn extends Column
     public function setDefaultActionsOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setRequired(array('route'));
-        
+
         $resolver->setOptional(array(
             'route_param',
             'disabled',
@@ -201,16 +209,16 @@ class ActionsColumn extends Column
         $resolver->setAllowedTypes(array(
             'required_roles' => 'array'
         ));
-        
+
         $resolver->setDefaults(array(
             'attributes' => array('style' => 'text-align: center;', 'class' => ''),
-            'disabled' => false            
+            'disabled' => false
         ));
     }
 
     /**
      * {@inheritdoc}
-     */ 
+     */
     public function getType()
     {
         return 'actions';
